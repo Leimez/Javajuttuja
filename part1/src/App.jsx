094222ad10a570505baaa7c269c52078e621c597
Leimez <leevikempas@gmail.com>
2025-01-20 10:28:46 +0200

    1.3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -22,11 +22,12 @@ const Total = ({ parts }) => (
 
 const App = () => {
   const course = 'Half Stack application development';
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 },
-  ];
+  const part1 = { name: 'Fundamentals of React', exercises: 10 };
+  const part2 = { name: 'Using props to pass data', exercises: 7 };
+  const part3 = { name: 'State of a component', exercises: 14 };
+
+  // Osat yhdistetään listaksi
+  const parts = [part1, part2, part3];
 
   return (
     <div>
@@ -38,4 +39,5 @@ const App = () => {
 };
 
 export default App;
+
 
