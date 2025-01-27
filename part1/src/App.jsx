f995d5625b926991f15bbac6c29ae734ef29f54a
Leimez <leevikempas@gmail.com>
2025-01-27 20:01:20 +0200

    1.9

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,6 +1,5 @@
-
 import { useState } from 'react';
-import Statistics from './Statistics'; 
+import Statistics from './Statistics';
 
 const App = () => {
   const [good, setGood] = useState(0);
--- part1/src/Statistics.jsx
+++ part1/src/Statistics.jsx
@@ -1,15 +1,16 @@
 const Statistics = ({ good, neutral, bad, all, average, positive }) => {
-    return (
-      <div>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {all}</p>
-        <p>average {average}</p>
-        <p>positive {positive} %</p>
-      </div>
-    );
-  };
-  
-  export default Statistics;
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  );
+};
+
+export default Statistics;
+
   
